0b152557a3c58a37a2276d1721fce863ac8df5e9
Yunus Andréasson <yunus@edenmind.com>
2022-05-28 23:52:16 +0200

    remove token
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

--- main.tf
+++ main.tf
@@ -15,10 +15,6 @@ terraform {
   }
 }
 
-# Set the variable value in *.tfvars file
-# or using -var="do_token=..." CLI option
-variable "do_token" {}
-
 # Configure the DigitalOcean Provider
 provider "digitalocean" {
   token = var.do_token
