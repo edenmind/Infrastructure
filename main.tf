8b6884a57ec0742c47ab7b449ca9c805028e157a
Yunus Andréasson <yunus@edenmind.com>
2022-05-29 18:11:38 +0200

    clean
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

--- main.tf
+++ main.tf
@@ -27,18 +27,12 @@ provider "digitalocean" {
 resource "digitalocean_kubernetes_cluster" "openarabic" {
   name   = "openarabic"
   region = "ams3"
-  # Grab the latest version slug from `doctl kubernetes options versions`
+  # Grab the latest version: `doctl kubernetes options versions`
   version = "1.22.8-do.1"
 
   node_pool {
     name       = "worker-pool"
     size       = "s-2vcpu-2gb"
     node_count = 3
-
-    taint {
-      key    = "workloadKind"
-      value  = "database"
-      effect = "NoSchedule"
-    }
   }
 }
