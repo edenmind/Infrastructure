34592e727be009aaf87d1afb96a6f3f671170839
Yunus Andréasson <yunus@edenmind.com>
2022-06-15 21:52:16 +0200

    .
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

--- main.tf
+++ main.tf
@@ -2,14 +2,9 @@ terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-       version = "2.20.0"
+      version = "2.20.0"
     }
   }
-}
-
-provider "digitalocean" {
-  # Configuration options
-}
 
   cloud {
     organization = "edenmind"
