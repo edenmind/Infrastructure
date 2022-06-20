0d3078c02acdaed04fba4588aae2933a75be8c63
Yunus Andréasson <yunus@edenmind.com>
2022-06-20 21:48:20 +0200

    switch watch
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

--- charts/main.tf
+++ charts/main.tf
@@ -15,11 +15,6 @@ resource "helm_release" "mongo-operator" {
   version          = "0.7.4"
   create_namespace = true
   namespace        = "mongodb-database"
-
-  set {
-    name  = "operator.watchNamespace"
-    value = "openarabic"
-  }
 }
 
 resource "helm_release" "istio-base" {
