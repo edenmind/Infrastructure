f123fa38ab21672ca505d77cfba27015ab0e5b5c
Yunus Andréasson <yunus@edenmind.com>
2022-10-24 22:25:00 +0200

    fix: :rocket: deploy without helm charts
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

--- main.tf
+++ main.tf
@@ -24,41 +24,44 @@ variable "slack_webhook" {}
 provider "digitalocean" {
   token = var.do_token
 }
-
-data "digitalocean_kubernetes_cluster" "openarabic" {
-  name = "openarabic"
-}
-
-provider "kubernetes" {
-  host  = data.digitalocean_kubernetes_cluster.openarabic.endpoint
-  token = data.digitalocean_kubernetes_cluster.openarabic.kube_config[0].token
-  cluster_ca_certificate = base64decode(
-    data.digitalocean_kubernetes_cluster.openarabic.kube_config[0].cluster_ca_certificate
-  )
-}
-
-provider "helm" {
-  kubernetes {
-    host  = data.digitalocean_kubernetes_cluster.openarabic.endpoint
-    token = data.digitalocean_kubernetes_cluster.openarabic.kube_config[0].token
-    cluster_ca_certificate = base64decode(
-      data.digitalocean_kubernetes_cluster.openarabic.kube_config[0].cluster_ca_certificate
-    )
-  }
-}
-
 resource "digitalocean_kubernetes_cluster" "openarabic" {
-  name   = "openarabic"
-  region = "ams3"
-  # Grab the latest version: `doctl kubernetes options versions`
-  version = "1.24.4-do.0"
+  name          = "openarabic"
+  region        = "ams3"
+  auto_upgrade  = true
+  surge_upgrade = true
+  version       = "1.24.4-do.0" # Grab the latest version: `doctl kubernetes options versions`
 
   node_pool {
     name       = "worker-pool"
     size       = "s-4vcpu-8gb"
     node_count = 1
+    auto_scale = true
+    min_nodes  = 1
+    max_nodes  = 2
   }
 }
+
+# data "digitalocean_kubernetes_cluster" "openarabic" {
+#   name = "openarabic"
+# }
+
+# provider "kubernetes" {
+#   host  = data.digitalocean_kubernetes_cluster.openarabic.endpoint
+#   token = data.digitalocean_kubernetes_cluster.openarabic.kube_config[0].token
+#   cluster_ca_certificate = base64decode(
+#     data.digitalocean_kubernetes_cluster.openarabic.kube_config[0].cluster_ca_certificate
+#   )
+# }
+
+# provider "helm" {
+#   kubernetes {
+#     host  = data.digitalocean_kubernetes_cluster.openarabic.endpoint
+#     token = data.digitalocean_kubernetes_cluster.openarabic.kube_config[0].token
+#     cluster_ca_certificate = base64decode(
+#       data.digitalocean_kubernetes_cluster.openarabic.kube_config[0].cluster_ca_certificate
+#     )
+#   }
+# }
 
 resource "kubernetes_namespace" "openarabic" {
   metadata {
@@ -85,7 +88,7 @@ resource "digitalocean_container_registry" "repository" {
 }
 
 
-module "helm_charts" {
-  source        = "./charts"
-  slack_webhook = var.slack_webhook
-}
+# module "helm_charts" {
+#   source        = "./charts"
+#   slack_webhook = var.slack_webhook
+# }
